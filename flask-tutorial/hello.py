cb821ad93857ec27196424c74fb7881279aca72e
Abel <esung1129@gmail.com>
2019-06-30 11:59:35 +0900

    :art: add routing

--- flask-tutorial/hello.py
+++ flask-tutorial/hello.py
@@ -1,21 +1,30 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Index Page'
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
 
 @app.route('/user/<username>')
-def show_user_pofile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username="Abel ko"))
 
 
 @app.route('/post/<int:post_id>')
@@ -29,10 +38,17 @@ def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % subpath
 
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
 
+
 @app.route('/about')
 def about():
     return 'The about page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
