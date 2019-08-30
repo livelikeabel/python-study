68dc82b1eedf5e752630b2fedee3166b5ee4e600
abelko <esung1129@gmail.com>
2019-08-30 10:53:49 +0900

    :art: add sign_up, tweet api

--- clean-python/api/app.py
+++ clean-python/api/app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+app.tweets = []
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+
+    return jsonify(new_user)
+
+@app.route("/tweet", methods=["POST"])
+def tweet():
+    payload = request.json
+    user_id = int(payload["id"])
+    tweet = payload["tweet"]
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과했습니다', 400
+
+    app.tweets.append({
+	'user_id': user_id,
+	'tweet': tweet
+    })
+
+    return '', 200
