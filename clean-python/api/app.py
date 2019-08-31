50a1bb94cec1be225185f999418043bc6c2b6ed3
abelko <esung1129@gmail.com>
2019-08-31 23:56:11 +0900

    :art: add follow api

--- clean-python/api/app.py
+++ clean-python/api/app.py
@@ -36,3 +36,17 @@ def tweet():
     })
 
     return '', 200
+
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+	return '사용자가 존재하지 않습니다.', 400
+    
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+
+    return jsonify(user)
