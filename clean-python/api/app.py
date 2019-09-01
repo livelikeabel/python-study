1d5779964138bfa01162507275551a5103689a7a
abelko <esung1129@gmail.com>
2019-09-01 12:06:52 +0900

    :art:custormJSONEncoder 추가

--- clean-python/api/app.py
+++ clean-python/api/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+
+## Default JSON encoder는 set을 JSON으로 변환할 수 없다.
+## 그러므로 커스텀 엔코더를 작성해서 set을 list로 변환하여
+## JSON으로 변환 가능하게 해주어야 한다.
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+       
+        return JSONEncoder.default(self, obj)
+
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
+
 app.users = {}
 app.id_count = 1
 app.tweets = []
@@ -44,7 +58,7 @@ def follow():
     user_id_to_follow = int(payload['follow'])
 
     if user_id not in app.users or user_id_to_follow not in app.users:
-	return '사용자가 존재하지 않습니다.', 400
+	    return '사용자가 존재하지 않습니다.', 400
     
     user = app.users[user_id]
     user.setdefault('follow', set()).add(user_id_to_follow)
