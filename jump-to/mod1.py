cab66a51d42fb1e70a8222db9b7252275a32e7b1
abelko <esung1129@gmail.com>
2019-11-22 02:53:18 +0900

    study class

---
+++ jump-to/mod1.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+
+# __name__변수는 파이썬의 특별한 변수이다.
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(sub(4, 2))
---
+++ jump-to/module.py
@@ -0,0 +1,4 @@
+# 모듈: 함수나 클래스를 모아 놓은 파일
+
+from mod1 import *
+print(add(3, 4))
